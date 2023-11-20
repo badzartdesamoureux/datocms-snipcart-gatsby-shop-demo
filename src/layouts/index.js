be3d54b82d2a54c7625c8db678790d283c8a73a8
badzartdesamoureux <151160591+badzartdesamoureux@users.noreply.github.com>
2023-11-20 16:04:28 +0100

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,7 +18,7 @@ const Layout = ({ children, site, seo }) => {
                 </Link>
               </h1>
               <div className="Header__summary snipcart-summary snipcart-checkout">
-                <div className="Header__summary__title">🛍MON PANIER🛍</div>
+                <div className="Header__summary__title">🛒 MON PANIER 🛒</div>
                 <div className="Header__summary__line">
                   Nombre de produits:{' '}
                   <span className="snipcart-total-items"></span>
